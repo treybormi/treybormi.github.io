c44a369981cbd8fccf02ec563f5b47cc7e031036
treybormi <benisonsuja@gmail.com>
2021-04-14 16:20:30 +0530

    Update instagram.php

--- instagram.php
+++ instagram.php
@@ -1,18 +1,12 @@
 <?php
 $opts = array(
   'https'=>array(
-    'method'=>"POST",
-    'header'=>"client_id: 3239305216171949\r\n". 
-              "client_secret: 897ef1175c13374095aa66c1de13683d\r\n".
-              "grant_type: authorization_code\r\n".
-              "redirect_uri: https://treybormi.github.io\r\n".
-              "code: AQDrPjvRDGmJHyiViZYjAsOhtEHVIJqlcGKIEMGlRqkGWBkI1Wr8qUS5Fzk_ZkbMVCFf1Sfb7K7Rv189Gdj2nqFqpAKSfqfvCSVvkilqwLJveZdG7Kj_UdfM0aey_xd8-DgnyyGVRxoP052JhjpU__blIjw1QOlh9CK_IzTmGKaiLmlmfZT4F6ScbrqVGHn9VbK6bRxy07yeTAXeaZs0K3uHxq-gqC6MI5UXC0gYEZIxhg\r\n"
-  )
+    'method'=>"POST")
 );
 
 $context = stream_context_create($opts);
 
 // Open the file using the HTTP headers set above
-$file = file_get_contents('https://api.instagram.com/oauth/access_token', false, $context);
+$file = file_get_contents('https://api.instagram.com/oauth/access_token?client_id=3239305216171949&client_secret=897ef1175c13374095aa66c1de13683d&grant_type=authorization_code&redirect_uri=https://treybormi.github.io&code=IGQVJYZAGpPMndpVXJSbmZA3TmUtVlZA4bTk5NTRHRUZAXeGNxOVhteE9hbEZAZAT0hWaVJ0dDRWbEU3cFpPMUFad2hLdkwxcnBaYnh4Rkc2ZAjdrZAW9ZAcGptaEw2V3pDRmdPck85VFoydkhfT0VEbVQ2Q0xYQgZDZD', false, $context);
 echo $file;
 ?>
