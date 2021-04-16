8d772e7ae52aa9375b921502edca5f4f55ef4ad1
treybormi <benisonsuja@gmail.com>
2021-04-16 15:48:01 +0530

    commit on 'develStage'

--- instagramApi.php
+++ instagramApi.php
@@ -1,15 +1,5 @@
 <?php
-/*$opts = array(
-  'https'=>array(
-    'method'=>"POST")
-);
-
-$context = stream_context_create($opts);
-
-// Open the file using the HTTP headers set above
-$file = file_get_contents('https://api.instagram.com/oauth/access_token?client_id=3239305216171949&client_secret=897ef1175c13374095aa66c1de13683d&grant_type=authorization_code&redirect_uri=https://treybormi.github.io&code=IGQVJYZAGpPMndpVXJSbmZA3TmUtVlZA4bTk5NTRHRUZAXeGNxOVhteE9hbEZAZAT0hWaVJ0dDRWbEU3cFpPMUFad2hLdkwxcnBaYnh4Rkc2ZAjdrZAW9ZAcGptaEw2V3pDRmdPck85VFoydkhfT0VEbVQ2Q0xYQgZDZD', false, $context);
-echo $file;*/
-
-$result = file_get_contents('https://api.instagram.com/oauth/access_token?client_id=3239305216171949&client_secret=897ef1175c13374095aa66c1de13683d&grant_type=authorization_code&redirect_uri=https://treybormi.github.io&code=IGQVJYZAGpPMndpVXJSbmZA3TmUtVlZA4bTk5NTRHRUZAXeGNxOVhteE9hbEZAZAT0hWaVJ0dDRWbEU3cFpPMUFad2hLdkwxcnBaYnh4Rkc2ZAjdrZAW9ZAcGptaEw2V3pDRmdPck85VFoydkhfT0VEbVQ2Q0xYQgZDZD', false, $context);
-echo $result;
+$url = "https://graph.instagram.com/me?fields=id,username&access_token=IGQVJVWHRGOFZANWkJZAZAmJJWWFtdjZADd0I5Q2xuVUlwZAy1pSFJoTkFwWHVVTVJoS0ZAPZAWoyb29YWkVHOXRpUU9uZADh1dFk1MU1vdnc4dWNPUXVuaFVFS19mWlJ3ZA3B3MW16dV9ROEE5V05YSmxXRmx1NgZDZD";
+$result = file_get_contents($url);
+$result = json_decode($result,true);
 ?>
